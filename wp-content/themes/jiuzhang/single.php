480bd8d21fa6b5882db097e4fb47f4ea65ea8b55
kapeter <hfxsky@hotmail.com>
2017-07-27 17:36:06 +0800

    分享功能完成

--- wp-content/themes/jiuzhang/single.php
+++ wp-content/themes/jiuzhang/single.php
@@ -9,6 +9,8 @@
 
 	    	<?php while ( have_posts() ) : the_post(); update_post_caches($posts); ?>
 
+	    	<?php $link = get_post_meta($post->ID, 'wlink', true); ?>
+
 	    	<?php if ( has_post_thumbnail() ) : ?>
 	    		<div class="article-thumbnail">
 	    			<?php the_post_thumbnail(); ?>
@@ -20,7 +22,7 @@
 	    	<div class="article-info">
 	    		<p><?=get_post_meta($post->ID, 'writer', true) ?></p>
 				<p><?php the_time('Y.m.d') ?></p>
-				<p><a href="javascript:;"><i class="iconfont">&#xe616;</i></a></p>
+				<p><a href="javascript:;" id="share-btn"><i class="iconfont">&#xe616;</i></a></p>
 	        </div>
 	    	
 			<div class="article-content">
@@ -37,12 +39,59 @@
 
 	      	<?php endwhile; wp_reset_query(); ?>
 
-
-
 	    </div>
 
 	</div>
 
 </div>
 
+<div class="modal">
+	<div class="modal-body">
+		<div class="modal-header clearfix">
+			<ul class="modal-button">
+				<li>
+					<a href="javascript:;" class="modal-close" id="modal-close"><i class="iconfont">&#xe602;</i></a>
+				</li>
+			</ul>
+		</div>
+		<div class="modal-content clearfix">
+			<?php if (!empty($link)) : ?>
+				<div id="qrcode" class="qrcode"></div>
+			<?php endif; ?>
+			<div class="share-box">
+ 				<a class="sina" href="javascript:void((function(s,d,e,r,l,p,t,z,c){var%20f='http://v.t.sina.com.cn/share/share.php?appkey=2992571369',u=z||d.location,p=['&url=',e(u),'&title=',e(t||d.title),'&source=',e(r),'&sourceUrl=',e(l),'&content=',c||'gb2312','&pic=',e(p||'')].join('');function%20a(){if(!window.open([f,p].join(''),'mb',['toolbar=0,status=0,resizable=1,width=440,height=430,left=',(s.width-440)/2,',top=',(s.height-430)/2].join('')))u.href=[f,p].join('');};if(/Firefox/.test(navigator.userAgent))setTimeout(a,0);else%20a();})(screen,document,encodeURIComponent,'','','','','',''));"><i class="iconfont sina">&#xe731;</i></a> 
+				<p>新浪微博</p>
+			</div>
+			<div class="share-box">
+				<a class="douban" href="javascript:void(function(){var d=document,e=encodeURIComponent,s1=window.getSelection,s2=d.getSelection,s3=d.selection,s=s1?s1():s2?s2():s3?s3.createRange().text:'',r='https://www.douban.com/recommend/?url='+e(d.location.href)+'&title='+e(d.title)+'&sel='+e(s)+'&v=1',w=480,h=400,x=function(){if(!window.open(r,'douban','toolbar=0,resizable=1,scrollbars=yes,status=1,width='+w+',height='+h+',left='+(screen.width-w)/2+',top='+(screen.height-h)/2))location.href=r+'&r=1'};if(/Firefox/.test(navigator.userAgent)){setTimeout(x,0)}else{x()}})()"><i class="iconfont">&#xe654;</i></a>
+				<p>豆瓣</p>
+			</div>
+			<div class="share-box">
+				<a class="facebook" href="javascript:window.open('http://www.facebook.com/sharer.php?u='+encodeURIComponent(document.location.href)+'&amp;t='+encodeURIComponent(document.title),'_blank','toolbar=yes, location=yes, directories=no, status=no, menubar=yes, scrollbars=yes, resizable=no, copyhistory=yes, width=600, height=450,top=100,left=350');void(0)"><i class="iconfont">&#xe6a2;</i></a>
+				<p>Facebook</p>
+			</div>
+		</div>
+	</div>
+</div>
+
 <?php get_footer(); ?>
+<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/assets/js/qrcode.min.js"></script>
+<script type="text/javascript">
+
+	$(function () {
+		$('#share-btn').on('click',function () {
+			<?php if (!empty($link)) : ?>
+				document.getElementById("qrcode").innerHTML = '';
+				var qrcode = new QRCode(document.getElementById("qrcode"), {
+					width : 100,
+					height : 100
+				});
+				qrcode.makeCode('<?=$link ?>');
+			<?php endif; ?>
+			$('.modal').addClass('modal-active');
+		});
+		$('#modal-close').on('click',function () {
+			$('.modal').removeClass('modal-active');
+		});
+	})
+</script>
